30ef8947ef018ef24dcaab90346de2f6a9754abe
LESS14 <srstevestifler@gmail.com>
2023-09-24 04:24:26 -0500

    Diversas correções & Melhorias

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import Link from 'next/link';
-import Tooltip from '@mui/material/Tooltip';
-import { Helmet } from 'react-helmet';
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import Tooltip from "@mui/material/Tooltip";
+import { Helmet } from "react-helmet";
 
 export async function getStaticProps() {
-  const files = fs.readdirSync('posts');
+  const files = fs.readdirSync("posts");
 
   const posts = files.map((fileName) => {
-    const slug = fileName.replace('.md', '');
-    const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const slug = fileName.replace(".md", "");
+    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
     const { data: frontmatter } = matter(readFile);
     return {
       slug,
@@ -26,17 +26,17 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
       {posts.map(({ slug, frontmatter }) => (
         <div
           key={slug}
-          className='border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col'
+          className="border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col hover:opacity-95 hover:rounded-t-none"
         >
-                <Helmet>
-                <title>Home</title>
-                </Helmet>
+          <Helmet>
+            <title>Home</title>
+          </Helmet>
           <Link href={`/post/${slug}`}>
-            <a>
+            <a className='hover:no-underline'>
               <img
                 width={650}
                 height={340}
@@ -44,7 +44,7 @@ export default function Home({ posts }) {
                 src={`/${frontmatter.socialImage}`}
               />
               <Tooltip title={frontmatter.title} arrow>
-              <h1 className='p-4'>{frontmatter.title}</h1>
+                <h1 className="p-4">{frontmatter.title}</h1>
               </Tooltip>
             </a>
           </Link>
