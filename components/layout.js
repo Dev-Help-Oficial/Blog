9e8623985db58577fc03eb39ccdc8b17561fa38e
LESS14 <srstevestifler@gmail.com>
2023-09-27 18:27:37 -0500

    Atualiza para nova versão do Next.js e React/DOM

--- components/layout.js
+++ components/layout.js
@@ -1,24 +1,32 @@
-import Link from 'next/link';
 import HomeIcon from '@mui/icons-material/Home';
 import Tooltip from '@mui/material/Tooltip';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
+  const router = useRouter();
+
   return (
     <div className='flex flex-col min-h-screen'>
       <header className='bg-slate-900 mb-8 py-4'>
         <div className='container mx-auto flex justify-center'>
-          <Link href='/'>
-            <Tooltip title='Início'>
-            <a><HomeIcon className='cursor-pointer' /></a>
-            </Tooltip>
-          </Link>
-          <span className='mx-auto'>Blog</span>{' '}        
+          <Tooltip title='Início'>
+            <a
+              href='/'
+              onClick={(e) => {
+                e.preventDefault();
+                router.push('/');
+              }}
+            >
+              <HomeIcon className='cursor-pointer' />
+            </a>
+          </Tooltip>
+          <span className='mx-auto'>Dev Help Blog</span>{' '}        
         </div>
       </header>
       <main className='container mx-auto flex-1'>{children}</main>
       <footer className='bg-slate-900 mt-8 py-4'>
         <div className='container mx-auto flex justify-center'>
-          &copy; DevHelp - 2023
+          &copy; DevHelp - 2023 - 2024
         </div>
       </footer>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/globals.css';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   const Layout = dynamic(() => import('../components/layout'));
 
 
   return (
+    <div className='layout'>
+    <DefaultSeo 
+      title='Dev Help Blog'
+      description='Um Blog totalmente open-source onde artigos e tutorias sobre programação e assuntos relacionados serão postados, mirando principalmente novatos. Qualquer um pode postar seus próprios artigos!'
+    />
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </div>
   );
 }
 
