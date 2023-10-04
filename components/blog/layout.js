ffd62dabeee9321650e0f3672d8f3815e7a67904
LESS14 <srstevestifler@gmail.com>
2023-10-04 06:16:19 -0500

    Migração para React-Markdown e mais

--- components/blog/layout.js
+++ components/blog/layout.js
@@ -1,17 +1,3 @@
-/* 
-  LEGAL:
-    Licença Pública Mozilla, versão 2.0
-
-    Este software está licenciado sob a Licença Pública Mozilla, versão 2.0 (a "Licença"); você pode não usar este arquivo, exceto em conformidade com a Licença. Você pode obter uma cópia da Licença em https://www.mozilla.org/MPL/2.0/.
-
-    O(s) autor(es) detém(êm) os direitos autorais deste software. 2023 é o ano de criação deste software.
-
-    De acordo com os termos da Licença, você pode usar, modificar e distribuir este software de acordo com os termos da Licença.
-
-    Salvo disposição em contrário na Licença, o software é distribuído "COMO ESTÁ", SEM GARANTIAS OU CONDIÇÕES DE QUALQUER TIPO, expressas ou implícitas. Consulte a Licença para os detalhes específicos sobre as permissões e limitações sob a Licença.
-    ---
-    Desenvolvedor Original: LESS14(Felipe Maciel)
-*/
 import HomeIcon from '@mui/icons-material/Home';
 import Tooltip from '@mui/material/Tooltip';
 import { useRouter } from 'next/router';
@@ -21,7 +7,7 @@ export default function Layout({ children }) {
 
   return (
     <div className='flex flex-col min-h-screen'>
-      <header className='bg-slate-900 mb-8 py-4'>
+      <header className='fixed top-0 left-0 w-full bg-slate-900 py-4'>
         <div className='container mx-auto flex justify-center'>
           <Tooltip title='Início'>
             <a
@@ -37,8 +23,8 @@ export default function Layout({ children }) {
           <span className='mx-auto'>Dev Help Blog</span>{' '}        
         </div>
       </header>
-      <main className='container mx-auto flex-1'>{children}</main>
-      <footer className='bg-slate-900 mt-8 py-4'>
+      <main className='container mx-auto flex-1 mt-20'>{children}</main>
+      <footer className='bg-slate-900 py-4'>
         <div className='container mx-auto flex justify-center'>
           &copy; DevHelp - 2023 - 2024
         </div>
