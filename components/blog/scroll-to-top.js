ea65e687d82da596a678d3ac301d9e6ef1fba94e
LESS14 <srstevestifler@gmail.com>
2023-10-20 21:35:17 -0500

    Adiciona seção 'LEGAL' no código-fonte
    Co-authored-by: LESS14 <100519912+LESS14@users.noreply.github.com>

--- components/blog/scroll-to-top.js
+++ components/blog/scroll-to-top.js
@@ -1,3 +1,18 @@
+/* 
+  LEGAL:
+    Licença Pública Mozilla, versão 2.0
+
+    Este software está licenciado sob a Licença Pública Mozilla, versão 2.0 (a "Licença"); você pode não usar este arquivo, exceto em conformidade com a Licença. Você pode obter uma cópia da Licença em https://www.mozilla.org/MPL/2.0/.
+
+    O(s) autor(es) detém(êm) os direitos autorais deste software. 2023 é o ano de criação deste software.
+
+    De acordo com os termos da Licença, você pode usar, modificar e distribuir este software de acordo com os termos da Licença.
+
+    Salvo disposição em contrário na Licença, o software é distribuído "COMO ESTÁ", SEM GARANTIAS OU CONDIÇÕES DE QUALQUER TIPO, expressas ou implícitas. Consulte a Licença para os detalhes específicos sobre as permissões e limitações sob a Licença.
+    ---
+    Desenvolvedor Original: LESS14(Felipe Maciel)
+    Editor: TanF12
+*/
 import { useState, useEffect } from 'react';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
